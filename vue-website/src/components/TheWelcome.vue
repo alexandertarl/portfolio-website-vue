1ad41828d02d8764f1464c4b82db913ee5add56b
Alexander Tarling <tarlinga@cardiff.ac.uk>
2024-06-22 13:30:52 +0100

    first

---
+++ vue-website/src/components/TheWelcome.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #heading>Brief Introduction</template>
+    I'm a graduate Software Engineer with a passion for all things coding and spoken/foreign languages. 
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #heading>Coding Languages Experience</template>
+    Python
+    Java
+    Javascript and Typescript
+    C#
+  </WelcomeItem>
+</template>
