9ff333311e4c9339019067262a803aac52b22c48
Alexander Tarling <tarlinga@cardiff.ac.uk>
2024-07-03 15:12:05 +0100

    Changed jpg files to png

--- src/views/ArtPortfolioView.vue
+++ src/views/ArtPortfolioView.vue
@@ -5,16 +5,16 @@
     <h1>My Art Portfolio</h1>
     <h2 class="green">Traditional Art</h2>
     Painted using Oil or Acrylic Paint <br/>
-    <img class="digital" src="../assets/cat1.jpeg" alt="Drawing of a cat 1" />
-    <img class="digital" src="../assets/cat2.jpeg" alt="Drawing of a cat 2" />
-    <img class="digital" src="../assets/cat3.jpeg" alt="Drawing of a cat 3" />
-    <img class="digital" src="../assets/girl and cat.jpeg" alt="Drawing of a cat and woman" />
+    <img class="digital" src="../assets/cat1.png" alt="Drawing of a cat 1" />
+    <img class="digital" src="../assets/cat2.png" alt="Drawing of a cat 2" />
+    <img class="digital" src="../assets/cat3.png" alt="Drawing of a cat 3" />
+    <img class="digital" src="../assets/girl and cat.png" alt="Drawing of a cat and woman" />
   </WelcomeItem>
 
   <WelcomeItem>
     <h2 class="green">Digital Art</h2>
     Drawn using Clip Studio Paint or HeavyPaint <br/>
-    <img class="digital" src="../assets/cat.jpg" alt="Drawing of a cat" />
+    <img class="digital" src="../assets/cat.png" alt="Drawing of a cat" />
     <img class="digital" src="../assets/banana.png" alt="Drawing of a banana" />
     <img class="digital" src="../assets/conan.png" alt="Drawing of a man" />
     <img class="digital" src="../assets/girl.png" alt="Drawing of a woman" />
